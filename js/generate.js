2ce35797ff3bc721a0ffd1cc45be5723a39d2754
Mehedi Hasan <designwithmehedi@gmail.com>
2023-10-30 02:06:00 +0600

    Pin Matcher

--- js/generate.js
+++ js/generate.js
@@ -28,18 +28,39 @@ document.getElementById('calculator').addEventListener('click', function () {
     const typeNumbersField = document.getElementById('type-numbers');
     const prevtypeNumber = typeNumbersField.value;
     if (isNaN(number)) {
-         if(number === 'C'){
+        if (number === 'C') {
             typeNumbersField.value = '';
-         }
-         else if(number === '<'){
+        }
+        else if (number === '<') {
             const digits = prevtypeNumber.split('');
             digits.pop();
             const reminingDigits = digits.join('');
             typeNumbersField.value = reminingDigits;
-         }
+        }
     }
-    else {  
+    else {
         const newTypeNumber = prevtypeNumber + number;
         typeNumbersField.value = newTypeNumber;
+    }
+}) 
+
+//---------------------------------------------
+
+document.getElementById('verify-pin').addEventListener('click', function () {
+    const displayPinField = document.getElementById('display-pin');
+    const currentPin = displayPinField.value;
+
+    const typeNumberField = document.getElementById('type-numbers');
+    const typeNumber = typeNumberField.value;
+    //-------------------------------------------------------------
+    const pinSuccessMessage = document.getElementById('pin-success');
+    const pinWrong = document.getElementById('pin-wrong');
+    if (typeNumber === currentPin) { 
+        pinSuccessMessage.style.display = "block";
+        pinWrong.style.display = 'none'
+    }
+    else { 
+        pinWrong.style.display = 'block'
+        pinSuccessMessage.style.display = "none";
     }
 })
